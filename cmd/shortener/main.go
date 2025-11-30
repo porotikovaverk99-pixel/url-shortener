a273c0764e03ec5296a46396bbca9558866356bf
Veronika <vaporotiva@edu.hse.ru>
2025-11-30 14:00:56 +0300

    Добавлен main.go для инкремента 2

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,62 +1,16 @@
 package main
 
 import (
-	"net/http"
-	"io"
-	"math/rand"
-	"strings"
+	"url-shortener/internal/handlers"
+	"url-shortener/internal/storage"
+	"url-shortener/internal/server"
 )
 
-var urlStorage = map[string]string{}
-var lenID int = 6
-
-func urlAction(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		id := strings.TrimPrefix(r.URL.Path, "/")
-		if id == "" {
-			http.Error(w, "Bad Request", http.StatusBadRequest)
-			return
-		}
-		originalURL := urlStorage[id]
-		if originalURL == "" {
-			http.Error(w, "Bad Request", http.StatusBadRequest)
-			return
-		}
-		w.Header().Set("Location", originalURL)
-		w.WriteHeader(307)
-	} else if r.Method == http.MethodPost {
-		body, err := io.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "Bad Request", http.StatusBadRequest)
-			return
-		}
-		originalURL := string(body)
-		if originalURL == "" {
-			http.Error(w, "Bad Request", http.StatusBadRequest)
-			return
-		}
-		id := generateShortID(lenID)
-		urlStorage[id] = originalURL
-		w.WriteHeader(201)
-		w.Write([]byte("http://" + r.Host + "/" + id))
-	} else {
-		http.Error(w, "Bad Request", http.StatusBadRequest)
-	}
-}
-
-func generateShortID(l int) string {
-    const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-    result := make([]byte, l) 
-    for i := range result {
-        result[i] = chars[rand.Intn(len(chars))]
-    }
-    return string(result)
-}
-
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", urlAction)
-	err := http.ListenAndServe(":8080", mux)
+	storage := storage.NewMemoryStorage()
+	handler := handlers.URLHandler(storage)
+	server := server.New(handler)
+	err := server.Run()
 	if err != nil {
 		panic(err)
 	}
