69ec71069ab052043a0407e29c5860c731e0c354
Veronika <vaporotiva@edu.hse.ru>
2025-12-06 15:39:09 +0300

    increment 4

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"math/rand"
-    "time"
+
 	"github.com/porotikovaverk99-pixel/url-shortener/internal/handlers"
 	"github.com/porotikovaverk99-pixel/url-shortener/internal/storage"
 	"github.com/porotikovaverk99-pixel/url-shortener/internal/server"
@@ -10,8 +9,6 @@ import (
 )
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
-
 	cfg := config.ParseFlags()
 
 	storage := storage.NewMemoryStorage()
