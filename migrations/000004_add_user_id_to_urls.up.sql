7520b7b30810c2121730b485b931cbb51196e56e
Veronika <vaporotiva@edu.hse.ru>
2026-02-03 16:52:37 +0300

    .

--- migrations/000004_add_user_id_to_urls.up.sql
+++ migrations/000004_add_user_id_to_urls.up.sql
@@ -1,7 +1,11 @@
 -- migrations/000004_add_user_id_to_urls.up.sql
--- Добавление user_id в таблицу urls
+-- Добавление колонки без внешнего ключа
+ALTER TABLE urls ADD COLUMN user_id UUID;
+
+-- Явное создание внешнего ключа с именем и каскадным удалением
 ALTER TABLE urls 
-ADD COLUMN user_id UUID REFERENCES users(id) ON DELETE CASCADE;
+ADD CONSTRAINT fk_urls_user_id 
+FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
 
 -- Уникальный индекс: один пользователь - один original_url
 CREATE UNIQUE INDEX idx_urls_user_original_url 
